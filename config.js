92235e6014a0b3c0b5b26d44187d52dd0f9cb7e9
Ferryloy <141764436+Ferryloy@users.noreply.github.com>
2024-10-28 16:05:35 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@
 */
 
 global.prefa = ['','!','.',',','🐤','🗿']
-global.owner = ['2348187637779']
-global.ownMain = '2348187637779'
+global.owner = ['2347047787798']
+global.ownMain = '2347047787798'
 global.NamaOwner = 'Es Teams Tech' //gausah diganti
 global.sessionName = 'sessionnya'
 global.connect = true // Ubah Ke False Jika Ingin Menggunakan Qr Code
